2384597fa0b49a2f3ca37db0ada7e11456c0f1ef
KarenCandy1991 <158361573+KarenCandy1991@users.noreply.github.com>
2024-04-25 12:40:26 -0600

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -36,6 +36,7 @@ def login_user(request):
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
+    
     logout(request)
     data = {"username": ""}
     return JsonResponse(data)
