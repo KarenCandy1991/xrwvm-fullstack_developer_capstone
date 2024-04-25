91a0d8fdf136c4cb215e1680b6fb6536f08ff549
KarenCandy1991 <158361573+KarenCandy1991@users.noreply.github.com>
2024-04-24 20:02:35 -0600

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,21 +1,14 @@
 # Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-
-
-
 # Create your models here.
-
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
-
     def __str__(self):
         return self.name  # Return the name as the string representation
-
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
@@ -32,6 +25,5 @@ class CarModel(models.Model):
             MinValueValidator(2015)
         ])
     # Other fields as needed
-
     def __str__(self):
         return self.name  # Return the name as the string representation
