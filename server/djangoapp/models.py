a146365e83b13e9fd5c45134ef72c998e98f5420
KarenCandy1991 <158361573+KarenCandy1991@users.noreply.github.com>
2024-04-24 20:04:32 -0600

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,14 +1,14 @@
-# Uncomment the following imports before adding the Model code
+#Uncomment the following imports before adding the Model code
 from django.db import models
 #from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-# Create your models here.
+#Create your models here.
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # Other fields as needed
+    #Other fields as needed
     def __str__(self):
-        return self.name  # Return the name as the string representation
+        return self.name  #Return the name as the string representation
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
@@ -16,7 +16,7 @@ class CarModel(models.Model):
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
-        # Add more choices as required
+        #Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
@@ -24,6 +24,6 @@ class CarModel(models.Model):
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
-    # Other fields as needed
+    #Other fields as needed
     def __str__(self):
-        return self.name  # Return the name as the string representation
+        return self.name  #Return the name as the string representation
