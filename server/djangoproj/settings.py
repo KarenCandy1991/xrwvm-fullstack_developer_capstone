67b4ef499c4cafb3556b65ffe7ae71613f1b6396
KarenCandy1991 <158361573+KarenCandy1991@users.noreply.github.com>
2024-04-25 13:52:25 -0600

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,12 +28,15 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = 
+[
     'localhost',
-    'https://karengranado-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'https://karengranado-8000.theiadockernext-0-labs'+
+    '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://karengranado-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'https://karengranado-8000.theiadockernext-0-labs-'+
+    'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 REST_FRAMEWORK = {
