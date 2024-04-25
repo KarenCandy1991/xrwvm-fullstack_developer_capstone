a1676f6e4ee9d8aae7002326b30a40e4ae4fb747
KarenCandy1991 <158361573+KarenCandy1991@users.noreply.github.com>
2024-04-25 12:26:11 -0600

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -16,7 +16,7 @@ def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
-            params=params + key + "=" + value + "&"
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -25,9 +25,9 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:
         # If any error occurs
-        print("Network exception occurred")
+        print("Network exception occurred", e)
 
 # def analyze_review_sentiments(text):
 def analyze_review_sentiments(text):
