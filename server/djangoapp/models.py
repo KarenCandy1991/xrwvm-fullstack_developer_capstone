a95a3062f63af58912473abf477966de71e5c067
KarenCandy1991 <158361573+KarenCandy1991@users.noreply.github.com>
2024-04-25 11:31:22 -0600

    Update models.py prueba 1

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,31 +1,31 @@
 # This is a comment describing the purpose of the models.py file
 # It should start with a hash symbol followed by a space
 
-
 from django.db import models
-
-#from django.utils.timezone import now
-
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-#Create your models here.
+# Create your models here.
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    #Other fields as needed
+    
+    # Other fields as needed
+    
     def __str__(self):
-        return self.name  #Return the name as the string representation
-        
+        return self.name  # Return the name as the string representation
+
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
+    
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
-        #Add more choices as required
+        # Add more choices as required
     ]
+    
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
         validators=[
@@ -33,7 +33,7 @@ class CarModel(models.Model):
             MinValueValidator(2015)
         ])
     
-    #Other fields as needed
+    # Other fields as needed
     
     def __str__(self):
-        return self.name  #Return the name as the string representation
+        return self.name  # Return the name as the string representation
