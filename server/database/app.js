4aa16774eb7d8c7ce11e74c1312982035923824d
KarenCandy1991 <158361573+KarenCandy1991@users.noreply.github.com>
2024-04-25 11:24:01 -0600

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -87,31 +87,32 @@ app.get('/fetchDealer/:id', async (req, res) => {
 });
 
 //Express route to insert review
-app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
-  data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } );
-  lconst new_id = documents[0].id + 1;
+app.post('/insert_review', express.json(), async (req, res) => {
+  const data = req.body;
+  const documents = await Reviews.find().sort({ id: -1 });
+  const new_id = documents[0].id + 1;
 
   const review = new Reviews({
-		"id": new_id,
-		"name": data.name,
-		"dealership":  data.dealership,
-		"review":  data.review,
-		"purchase": d data.purchase,
-		"purchase_date":  data.purchase_dat,
-		"car_make":  data.car_make,
-		"car_model":  data.car_model,
-		"car_year":  data.car_year
-	});
+    "id": new_id,
+    "name": data.name,
+    "dealership": data.dealership,
+    "review": data.review,
+    "purchase": data.purchase,
+    "purchase_date": data.purchase_date, // Fixed typo here
+    "car_make": data.car_make,
+    "car_model": data.car_model,
+    "car_year": data.car_year
+  });
 
   try {
     const savedReview = await review.save();
     res.json(savedReview);
   } catch (error) {
-		console.log(error);
+    console.log(error);
     res.status(500).json({ error: 'Error inserting review' });
   }
 });
+
 
 // Start the Express server
 app.listen(port, () => {
