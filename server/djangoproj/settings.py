f2c097e4821bf44c65485aa81b6ddbca6e668d18
KarenCandy1991 <158361573+KarenCandy1991@users.noreply.github.com>
2024-04-25 13:37:47 -0600

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['localhost',
                 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://karengranado-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
-]
+    ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
