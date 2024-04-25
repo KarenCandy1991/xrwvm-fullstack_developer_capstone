8a66556f3596416780bb763cc7c586f95de1577b
KarenCandy1991 <158361573+KarenCandy1991@users.noreply.github.com>
2024-04-25 12:59:23 -0600

    Update views.py i

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -78,20 +78,21 @@ def registration(request):
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
+    else:
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
 
-def get_cars(request):
+def get_cars(request): 
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if(count == 0): 
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name, 
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
