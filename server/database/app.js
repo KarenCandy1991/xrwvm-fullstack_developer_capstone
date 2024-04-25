e25f762c7e4f1f435e24cccbc890ec8d0766c64b
KarenCandy1991 <158361573+KarenCandy1991@users.noreply.github.com>
2024-04-25 11:22:08 -0600

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -101,7 +101,7 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
 		"purchase_date":  data.purchase_dat,
 		"car_make":  data.car_make,
 		"car_model":  data.car_model,
-		"car_year":  data.car_year,
+		"car_year":  data.car_year
 	});
 
   try {
