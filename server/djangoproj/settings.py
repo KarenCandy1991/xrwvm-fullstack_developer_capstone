8028ecfd3ed58d7e45f57ce34d5dd42cb7bd75e9
KarenCandy1991 <158361573+KarenCandy1991@users.noreply.github.com>
2024-04-25 13:53:13 -0600

    Ultimo karen

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = 
-[
+ALLOWED_HOSTS = [
     'localhost',
     'https://karengranado-8000.theiadockernext-0-labs'+
     '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
