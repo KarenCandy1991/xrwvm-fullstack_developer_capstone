ab34a24f2bc017e0c8b2bb81b2ad5914c076c7cc
KarenCandy1991 <158361573+KarenCandy1991@users.noreply.github.com>
2024-04-25 11:42:33 -0600

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -6,6 +6,8 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
